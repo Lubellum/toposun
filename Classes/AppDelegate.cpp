567c89af10b902eb4a581dfc4952469b6e16d443
Lubellum <90450493+Lubellum@users.noreply.github.com>
2025-12-29 08:39:36 +0900

    TitleSceneの呼び出し

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -24,6 +24,7 @@
 
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
+#include "Title/TitleScene.h"
 
 // #define USE_AUDIO_ENGINE 1
 // #define USE_SIMPLE_AUDIO_ENGINE 1
@@ -119,7 +120,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+    auto scene = CTitleScene::createScene();
 
     // run
     director->runWithScene(scene);
