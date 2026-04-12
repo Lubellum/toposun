a895c7f5da8c39cfb2c601777afc2b628e31a86a
Lubellum <90450493+Lubellum@users.noreply.github.com>
2026-04-12 10:16:48 +0900

    プレイ結果画面
    シーンの追加

---
+++ Classes/Play/PlayResultScene.cpp
@@ -0,0 +1,99 @@
+﻿#include "PlayResultScene.h"
+#include "PlayScene.h"
+#include "SimpleAudioEngine.h"
+#include "ui/UIImageView.h"
+#include "ui/UIText.h"
+#include "cocostudio/CocoStudio.h"
+
+// todo: 消す
+USING_NS_CC;
+
+// ========================================================================= //
+// ゲーム内容確認画面
+// ========================================================================= //
+
+// ------------------------------------------------------------------------- //
+// 生成
+// ------------------------------------------------------------------------- //
+CPlayResultScene* CPlayResultScene::create(const std::string& aParameter)
+{
+    auto* instance = CPlayResultScene::create();
+    instance->Initilize(aParameter);
+    return instance;
+}
+
+// ------------------------------------------------------------------------- //
+// シーン生成
+// ------------------------------------------------------------------------- //
+Scene* CPlayResultScene::CreateScene(const std::string& aParameter)
+{
+    return CPlayResultScene::create(aParameter);
+}
+
+// ------------------------------------------------------------------------- //
+// コンストラクタ
+// ------------------------------------------------------------------------- //
+CPlayResultScene::CPlayResultScene()
+{
+}
+
+// ------------------------------------------------------------------------- //
+// デストラクタ
+// ------------------------------------------------------------------------- //
+CPlayResultScene::~CPlayResultScene()
+{
+}
+
+// ------------------------------------------------------------------------- //
+// 初期化
+// ------------------------------------------------------------------------- //
+bool CPlayResultScene::init()
+{
+    if ( Scene::init() == false )
+    {
+        return false;
+    }
+
+    return true;
+}
+
+// ------------------------------------------------------------------------- //
+// 更新
+// ------------------------------------------------------------------------- //
+void CPlayResultScene::update(float delta)
+{
+
+}
+
+// ------------------------------------------------------------------------- //
+// 初期化
+// ------------------------------------------------------------------------- //
+void CPlayResultScene::Initilize(const std::string& aParameter)
+{
+    mParameter = aParameter;
+    auto guiReader = cocostudio::GUIReader::getInstance();
+    auto root = guiReader->widgetFromJsonFile("json/playresult.json");
+    this->addChild(root);
+    auto image = dynamic_cast<cocos2d::ui::ImageView*>(root->getChildByName("image_bg"));
+    image->addClickEventListener(CreateDecisionEvent());
+    
+    auto textLevel = dynamic_cast<cocos2d::ui::Text*>(root->getChildByName("text_level"));
+    auto beforeSentence = textLevel->getString();
+    auto value = cocos2d::StringUtils::format(beforeSentence.c_str(), mParameter.c_str());
+    textLevel->setString(value);
+}
+
+// ------------------------------------------------------------------------- //
+// 決定イベント生成
+// ------------------------------------------------------------------------- //
+std::function<void(Ref*)> CPlayResultScene::CreateDecisionEvent()
+{
+    return [this](cocos2d::Ref*)
+        {
+            cocos2d::log(mParameter.c_str());
+            auto director = Director::getInstance();
+            auto scene = CPlayScene::CreateScene("eeeeeeeee");
+            auto transition = TransitionFade::create(0.5, scene);
+            director->replaceScene(transition);
+        };
+}
---
+++ Classes/Play/PlayResultScene.h
@@ -0,0 +1,29 @@
+﻿#ifndef PLAYRESULTSCENE_H
+#define PLAYRESULTSCENE_H
+
+#include "cocos2d.h"
+
+// ========================================================================= //
+// ゲーム内容確認画面
+// ========================================================================= //
+class CPlayResultScene : public cocos2d::Scene
+{
+    CREATE_FUNC(CPlayResultScene);
+    static CPlayResultScene* create(const std::string& aParameter);
+public:
+    static cocos2d::Scene* CreateScene(const std::string& aParameter);
+    
+    CPlayResultScene();
+    virtual ~CPlayResultScene();
+
+    virtual bool init() override;
+    virtual void update(float delta) override;
+    
+    void Initilize(const std::string& aParameter);
+    std::function<void(Ref*)> CreateDecisionEvent();
+
+private:
+    std::string mParameter;
+};
+
+#endif // PLAYRESULTSCENE_H
