5a882ac95973856f1a6a62fdf2b7adc8f3649f23
Lubellum <90450493+Lubellum@users.noreply.github.com>
2026-07-26 20:20:30 +0900

    タイルマップ仮データ作成
    タイルサイズを24×24pxから40×40pxに変更

--- Resources/map/terrain.tsx
+++ Resources/map/terrain.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="terrain" tilewidth="24" tileheight="24" tilecount="16" columns="16">
- <image source="terrain.png" width="384" height="24"/>
+<tileset version="1.10" tiledversion="1.12.2" name="terrain" tilewidth="40" tileheight="40" tilecount="16" columns="16">
+ <image source="terrain.png" width="640" height="40"/>
 </tileset>
