158f9fa5ed3096483dd04c2d8e7ac1315795b404
Lubellum <90450493+Lubellum@users.noreply.github.com>
2026-07-26 20:01:15 +0900

    タイルマップ仮データ作成
    tsxファイルを更新対象に追加

---
+++ Resources/map/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="terrain" tilewidth="24" tileheight="24" tilecount="16" columns="16">
+ <image source="terrain.png" width="384" height="24"/>
+</tileset>
