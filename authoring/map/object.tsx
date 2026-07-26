3db6cac4233d4536c6e51bd1d88ed78dfeb820df
Lubellum <90450493+Lubellum@users.noreply.github.com>
2026-07-26 19:44:38 +0900

    タイルマップ仮データ作成

---
+++ authoring/map/object.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="object" tilewidth="24" tileheight="24" tilecount="5" columns="5">
+ <image source="object.png" width="120" height="24"/>
+</tileset>
---
+++ copy_tmx_png.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: --- 設定エリア ---
+set "SOURCE_ROOT=C:\Project\GitHub\Toposun\toposun\authoring\map"
+set "DEST_PATH=C:\Project\GitHub\Toposun\toposun\Resources\map"
+
+echo マップ関連ファイルのコピー処理を開始します...
+echo.
+
+:: 1. 出力先フォルダの存在チェック
+if not exist "%DEST_PATH%" (
+    echo 【エラー】出力先フォルダが見つかりません。
+    echo 対象パス: "%DEST_PATH%"
+    echo 処理を中断します。
+    pause
+    exit /b
+)
+
+:: 2. .tmx ファイルと .png ファイルのコピー実行（上書き許可）
+echo .tmx ファイルをコピー中...
+copy /Y "%SOURCE_ROOT%\*.tmx" "%DEST_PATH%\"
+
+echo .png ファイルをコピー中...
+copy /Y "%SOURCE_ROOT%\*.png" "%DEST_PATH%\"
+
+echo.
+echo すべてのコピー処理が完了しました。
+pause
